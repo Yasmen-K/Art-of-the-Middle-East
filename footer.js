dad76dcff5ff7b6369d3b0909b8b3306a4d8c066
Yasmen-K <kodjabashevayasemin@gmail.com>
2021-05-04 16:53:49 +0300

    Reusable Footer

---
+++ footer.js
@@ -0,0 +1,22 @@
+function createFooter(){
+
+
+
+    let footerElement = document.createElement('footer')
+    let footerUl = document.createElement('ul')
+    let liInstagram = document.createElement('li')
+    let aInstagram = document.createElement('a')
+    let imgInstagram = document.createElement('img')
+    
+    imgInstagram.src = "images/instagram.png"
+    aInstagram.appendChild(imgInstagram)
+    liInstagram.appendChild(aInstagram)
+    footerUl.appendChild(liInstagram)
+    footerElement.appendChild(footerUl)
+    document.body.appendChild(footerElement)
+
+}
+
+createFooter()
+
+
